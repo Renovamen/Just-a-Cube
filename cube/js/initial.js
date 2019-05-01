dff83080f2aa4ff7c82f9823673b772696e9c9d1
Renovamen <renovamenzxh@gmail.com>
2019-05-01 17:49:13 +0800

    [Refactor] function updateCubeTwoPhase()

--- cube/js/initial.js
+++ cube/js/initial.js
@@ -72,60 +72,6 @@ $(document).ready( function(){
 		cubeGL.rotation.z += fixedOrientation.z;
 	}
 
-	cubeGL.addEventListener("onTwistComplete", () => {
-		updateCubeTwoPhase()
-	})
-
-	// ------------- 同步 cuber 和 cubejs 的魔方状态
-	function updateCubeTwoPhase()
-	{
-		var cubeStringColor = ""
-		var cubeStringFaces = ""
-		
-		var read = [8, 7, 6, 5, 4, 3, 2, 1, 0]
-		for (var i = 0; i <= 8; i++) {
-			cubeStringColor += cubeGL.up.cubelets[read[i]].up.color.initial
-		}
-		for (var i = 0; i <= 8; i++) {
-			cubeStringColor += cubeGL.right.cubelets[read[i]].right.color.initial
-		}
-		for (var i = 0; i <= 8; i++) {
-			cubeStringColor += cubeGL.front.cubelets[read[i]].front.color.initial
-		}
-
-		read = [2, 5, 8, 1, 4, 7, 0, 3, 6]
-		for (var i = 0; i <= 8; i++) {
-			cubeStringColor += cubeGL.down.cubelets[read[i]].down.color.initial
-		}
-
-		read = [6, 3, 0, 7, 4, 1, 8, 5, 2]
-		for (var i = 0; i <= 8; i++) {
-			cubeStringColor += cubeGL.left.cubelets[read[i]].left.color.initial
-		}
-		for (var i = 0; i <= 8; i++) {
-			cubeStringColor += cubeGL.back.cubelets[read[i]].back.color.initial
-		}
-
-		var cubeOriginColors = [
-			{pos: "U", color: cubeGL.up.cubelets[4].up.color.initial},
-			{pos: "R", color: cubeGL.right.cubelets[4].right.color.initial},
-			{pos: "F", color: cubeGL.front.cubelets[4].front.color.initial},
-			{pos: "D", color: cubeGL.down.cubelets[4].down.color.initial},
-			{pos: "L", color: cubeGL.left.cubelets[4].left.color.initial},
-			{pos: "B", color: cubeGL.back.cubelets[4].back.color.initial}
-		]
-
-		for (var i = 0; i < cubeStringColor.length; i++) {
-			for (var j = 0; j < cubeOriginColors.length; j++) {
-				if(cubeStringColor[i] == cubeOriginColors[j].color) {
-					cubeStringFaces += cubeOriginColors[j].pos
-					break
-				}
-			}
-		}
-		cubeTwoPhase = Cube.fromString(cubeStringFaces)
-	}
-
 	// The deviceMotion function provide some subtle mouse based motion
 	// The effect can be used with the Freeform and Locked controls.
 	// This could also integrate device orientation on mobile
